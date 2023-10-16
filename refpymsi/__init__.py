1a20636f56c99fb3751a2fe2ffdb45b33079bfbc
GuillaumePressiat <guillaume.pressiat@gmail.com>
2023-10-16 19:14:02 +0200

    dictionnaire de listes

--- refpymsi/__init__.py
+++ refpymsi/__init__.py
@@ -3,10 +3,10 @@
 
 
 from refpymsi.tables.get_table import get_table
-#from refpypmsi.tables.get_dictionnaire_tables import get_dictionnaire_tables
+#from refpymsi.tables.get_dictionnaire_tables import get_dictionnaire_tables
 
 
 from refpymsi.listes.get_liste import get_liste
-#from refpypmsi.listes.get_dictionnaire_listes import get_dictionnaire_listes
+from refpymsi.listes.get_liste import get_dictionnaire_listes
 
 from refpymsi.utils import get_data_path
--- refpymsi/listes/get_liste.py
+++ refpymsi/listes/get_liste.py
@@ -4,6 +4,8 @@
 import gzip
 import json
 import pprint
+import pandas
+import polars as pl
 
 from refpymsi.utils import get_data_path
 
@@ -12,5 +14,17 @@ def get_liste(nom_liste, plrs = True):
     with open(get_data_path() + "/listes/" + nom_liste + ".json") as json_file: 
                    data = json.load(json_file)
     
+    return(data)
+
+
+def get_dictionnaire_listes(plrs = True):
+    with open(get_data_path() + "/dictionnaire.json") as json_file: 
+                   data = json.load(json_file)
+    
+    d =  pandas.DataFrame(data)
+
+    if plrs:
+        return(pl.from_pandas(d))
+
     return(data)
 
